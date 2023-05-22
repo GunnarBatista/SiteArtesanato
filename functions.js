bea53f40f72c1297ea9f720480d7af4fb0eab90a
Gunnar Batista <gunnar-batista@hotmail.com>
2023-05-21 23:23:33 -0300

    projeto finalizado

---
+++ functions.js
@@ -0,0 +1,8 @@
+function alerta() {
+      Swal.fire({
+        title: 'Adicionado',
+        text: 'Produto adicionado no carrinho com sucesso.',
+        icon: 'success',
+        confirmButtonText: 'OK'
+  });
+}
